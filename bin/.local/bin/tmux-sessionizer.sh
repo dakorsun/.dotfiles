edb4cbd63978432e03fc72da6975c5932d2068cc
Danylo Korsun <dakorsun@gmail.com>
2023-04-11 19:47:37 +0300

    tmux-sessionizer: More accurate search for projects

--- bin/.local/bin/tmux-sessionizer.sh
+++ bin/.local/bin/tmux-sessionizer.sh
@@ -4,8 +4,20 @@ if [[ $# -eq 1 ]]; then
     selected=$1
 else
 	selected=$({ 
+		# gently search through bare repo
+		find ~/.dotfiles.git/*  \
+		-maxdepth 0 -type d  \
+		-not -path '*branches*'  \
+		-and -not -path '*worktrees*'  \
+		-and -not -path '*info*' \
+		-and -not -path '*objects*' \
+		-and -not -path '*description*' \
+		-and -not -path '*config*' \
+		-and -not -path '*hooks*' \
+		-and -not -path '*logs*' \
+		-and -not -path '*refs*' ;
 		find ~/.dotfiles ~/.dotfiles.git ~/.dotfiles.git/*/nvim -maxdepth 0 -type d ;
-		find ~/projects ~/work ~/personal ~/personal/junk ~/personal/tutors -mindepth 1 -maxdepth 1 -type d ;
+		find ~/projects/* ~/work ~/personal ~/personal/junk ~/personal/tutors -mindepth 1 -maxdepth 1 -type d ;
 	} | fzf )
 fi
 
