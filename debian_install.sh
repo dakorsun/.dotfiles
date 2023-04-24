54b456d67230cfe34fd0de3392f378e989ae59a5
Danylo Korsun <dakorsun@gmail.com>
2023-04-24 16:04:49 +0300

    Debian: first load pack of required packages

--- debian_install.sh
+++ debian_install.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
 
-# git vim
-apt install git vim zsh ;
+# git vim curl wget unzip 
+apt install git vim curl wget unzip ;
+
+# ZSH + OH MY ZSH and around
+apt install zsh ; 
+
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/3270.zip" -O $USER_HOME/Downloads/Nerd_3270.zip ;
+mkdir ~/.fonts && mkdir ~/.fonts/Nerd_Fonts ;
+unzip ~/Downloads/Nerd_3270.zip -d ~/.fonts/Nerd_Fonts ;
+fc-cache -fv ;
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ;
+
+chsh -s /bin/zsh ;
+
+# Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage ;
+chmod u+x nvim.appimage ;
+alias nvim='~/nvim.appimage' ;
+
