894e39c8c9d87b8297d4c41655983eb23d0325f3
Danylo Korsun <dakorsun@gmail.com>
2024-10-24 17:03:18 +0300

    updated stow, removed redundant script

--- stow.sh
+++ stow.sh
@@ -1,4 +1,26 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+
+mkdir ~/personal/ ~/personal/junk/ ~/personal/tutors/ ;
+mkdir ~/projects/ ;
+mkdir ~/work/ ;
+mkdir ~/.config/fontconfig/ ~/.config/fontconfig/conf.d/ ;
+
+
+# --- Check and install Stow ---
+if ! command -v stow &> /dev/null
+then
+    echo "Stow is not installed. Installing Stow..."
+    sudo apt update
+    sudo apt install -y stow
+else
+    echo "Stow is already installed."
+fi
+
+# --- Proceed with dotfiles stowing ---
+STOW_FOLDERS="bin,nvim,tmux,debian,zsh"
+DOTFILES="$HOME/.dotfiles"
+
+echo "Stowing dotfiles from $DOTFILES"
 
 pushd $DOTFILES
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
@@ -7,3 +29,4 @@ do
     # stow -D $folder
     stow $folder
 done
+popd
