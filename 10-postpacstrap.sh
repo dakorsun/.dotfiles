52ac40110c765e06dce973fcdada5cec9ec03ef8
dk <dakorsun@gmail.com>
2025-11-27 20:39:56 +0200

    interim  branch change

--- 10-postpacstrap.sh
+++ 10-postpacstrap.sh
@@ -24,4 +24,4 @@ sed -i 's/^# %wheel/%wheel/' /etc/sudoers
 
 su - "$user" -c 'git clone https://aur.archlinux.org/paru.git ~/paru && cd ~/paru && makepkg -si --noconfirm'
 
-su - "$user" -c 'git clone https://github.com/dakorsun/.dotfiles.git ~/.dotfiles || (cd ~/.dotfiles && git pull)'
+su - "$user" -c 'git clone -b feature/new-version https://github.com/dakorsun/.dotfiles.git ~/.dotfiles || (cd ~/.dotfiles && git pull)'
