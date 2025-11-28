d64526790e025f520200dd314c42fb3bbf6e9f1d
dk <dakorsun@gmail.com>
2025-11-28 20:28:54 +0200

    updated fonts

--- 10-fonts.sh
+++ 10-fonts.sh
@@ -8,18 +8,18 @@ mkdir -p "$FONT_DIR"
 TMP_DIR=$(mktemp -d)
 cd "$TMP_DIR"
 
+ARCHIVE_NAME="Mononoki.tar.xz"
+
 echo ""
-echo "Downloading Mononoke Nerd Font"
-FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Mononoki.tar.xz"
+echo "Downloading Mononoki Nerd Font"
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$ARCHIVE_NAME"
 curl -OL "$FONT_URL"
 
-tar -xz Mononoki.tar.xz
+tar -xf $ARCHIVE_NAME -O "$FONT_DIR"
 
 fc-cache -fv
 
 cd ~
 rm -rf "$TMP_DIR"
 
-echo "Mononoke Nerd Font, gotcha!"
-
- 
+echo "Mononoki Nerd Font, gotcha!"
