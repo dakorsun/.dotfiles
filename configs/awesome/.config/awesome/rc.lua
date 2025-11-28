1fdcebe24bf99bea13bcf18bae0b4c6adc0d25a9
dk <dakorsun@gmail.com>
2025-11-28 18:56:35 +0200

    no floating for sure

--- configs/awesome/.config/awesome/rc.lua
+++ configs/awesome/.config/awesome/rc.lua
@@ -66,7 +66,7 @@ modkey = "Mod1"
 -- @DOC_LAYOUT@
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
+    -- awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
