f687d2f94094fa8876476881a431c1cc8973cdab
dk <dakorsun@gmail.com>
2025-12-11 21:02:27 +0200

    additional changes to nnn

--- configs/bash/.nnn.sh
+++ configs/bash/.nnn.sh
@@ -1,3 +1,4 @@
-export NNN_PLUG='f:finder;c:fzcd;o:fzopen;p:preview-tui;d:diffs;t:nmount;v:imgview'
+export NNN_PLUG='c:fzcd;o:fzopen;p:preview-tui;v:imgview'
+export NNN_TERMINAL='alacritty'
 alias ls='nnn -de'
 export NNN_FIFO='/tmp/nnn.fifo'
