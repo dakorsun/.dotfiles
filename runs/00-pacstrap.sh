c9f43040c1b8613f7800e31c69944556b8fc2890
dk <dakorsun@gmail.com>
2026-01-03 18:46:06 +0200

    Revert "lets try a better pacstrap way"
    This reverts commit 132964324efc59fe1cf1d89de5a23265a0d687bf.

--- runs/00-pacstrap.sh
+++ runs/00-pacstrap.sh
@@ -4,12 +4,9 @@ set -e
 rm -rf /mnt/.dotfiles
 git clone -b feature/new-version https://github.com/dakorsun/.dotfiles.git /mnt/.dotfiles
 
-mapfile -t PACKAGES < <(grep -vE '^\s*(#|$)' "$PWD/pacstrap.list")
+pacstrap /mnt base linux linux-firmware $(cat /mnt/.dotfiles/pacstrapList.txt)
 
-pacstrap /mnt base linux linux-firmware \
-    $"${PACKAGES[@]}"
-
-genfstab -U /mnt >>/mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
 cp /mnt/.dotfiles/runs/01-postpacstrap.sh /mnt/root/
 chmod +x /mnt/root/01-postpacstrap.sh
@@ -20,3 +17,4 @@ echo "arch-chroot /mnt"
 echo "and then set the root password: "
 echo "passwd"
 echo "then generate locales and run ~/01-postpacstrap.sh"
+
