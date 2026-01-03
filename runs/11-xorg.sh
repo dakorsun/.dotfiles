08124b61cea8fcce40d06adfde3c8fb21fa0a0a0
dk <dakorsun@gmail.com>
2026-01-03 17:49:37 +0200

    added resolution setting for xorg

--- runs/11-xorg.sh
+++ runs/11-xorg.sh
@@ -4,3 +4,12 @@ sudo pacman -S --needed \
     xorg-server xorg-xinit xorg-xrandr \
     xterm xclip xorg-xdpyinfo
 
+
+DPI=$(xdpyinfo | awk '/resolution:/ {split($2,a,"x"); print a[1]}')
+
+if [[ $DPI =~ ^[0-9]+$ ]]; then
+  echo "DPI = $DPI"
+  xrandr --dpi $DPI
+else
+  echo "DPI not detected"
+fi
