132964324efc59fe1cf1d89de5a23265a0d687bf
dk <dakorsun@gmail.com>
2026-01-03 18:43:00 +0200

    lets try a better pacstrap way

--- runs/00-pacstrap.sh
+++ runs/00-pacstrap.sh
@@ -4,9 +4,12 @@ set -e
 rm -rf /mnt/.dotfiles
 git clone -b feature/new-version https://github.com/dakorsun/.dotfiles.git /mnt/.dotfiles
 
-pacstrap /mnt base linux linux-firmware $(cat /mnt/.dotfiles/pacstrapList.txt)
+mapfile -t PACKAGES < <(grep -vE '^\s*(#|$)' "$PWD/pacstrap.list")
 
-genfstab -U /mnt >> /mnt/etc/fstab
+pacstrap /mnt base linux linux-firmware \
+    $"${PACKAGES[@]}"
+
+genfstab -U /mnt >>/mnt/etc/fstab
 
 cp /mnt/.dotfiles/runs/01-postpacstrap.sh /mnt/root/
 chmod +x /mnt/root/01-postpacstrap.sh
@@ -17,4 +20,3 @@ echo "arch-chroot /mnt"
 echo "and then set the root password: "
 echo "passwd"
 echo "then generate locales and run ~/01-postpacstrap.sh"
-
